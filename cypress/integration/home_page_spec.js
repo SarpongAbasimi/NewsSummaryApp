bdf3fea42d0fcdd0e6c5afbe9c0f0eaf3c96c57e
sarpong <abasimisarpong@rocketmail.com>
2019-06-08 18:15:06 +0100

    resolved failing test

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -10,11 +10,3 @@ describe('App', ()=>{
     cy.contains('Find out more')
   });
 });
-
-describe('App', ()=>{
-  it('It works',()=>{
-    cy.visit('/')
-    cy.contains('Find out more').click()
-    cy.url().should('include', 'https://www.theguardian.com')
-  });
-});
