915bf905055e1942449e0a07f98106fe45e27a82
sarpong <abasimisarpong@rocketmail.com>
2019-06-16 16:49:54 +0100

    refactored componentdidmount

--- src/components/body.js
+++ src/components/body.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { apiKey } from '../config';
 import { News } from '../components/news';
-//import { apiCall } from '../components/apicall';
+import { fetchData } from '../components/apicall';
 
 export class Body extends React.Component{
   constructor(props){
@@ -13,16 +13,7 @@ export class Body extends React.Component{
 
   componentDidMount(){
     const uri = apiKey.guardian;
-    fetch(uri)
-    console.log('fecthing.......')
-    .then(response => {
-      if(response.ok){
-        return response.json()
-      }
-      throw new Error ('Failed Request')
-    }, networkError => console.log(networkError.message))
-    .then(response=> response.response.results.map(eachData=>eachData))
-    .then(list => this.setState({list}))
+    return fetchData(uri, this)
   }
 
   render(){
