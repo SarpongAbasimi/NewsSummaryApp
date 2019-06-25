469d1a954bc672ae1122f0d8931f9d6b490055e5
sarpong <abasimisarpong@rocketmail.com>
2019-06-25 17:12:18 +0100

    Updates

---
+++ precache-manifest.639a53419face62a5b992b374cf47bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2d01428573f1f324dffd0b9a19a0f2",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "8ee132b8a26a16808bae",
+    "url": "/myapp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c7a52cf5c1c4da019ea5",
+    "url": "/myapp/static/css/main.edb255e7.chunk.css"
+  },
+  {
+    "revision": "8ee132b8a26a16808bae",
+    "url": "/myapp/static/js/2.d17c99e2.chunk.js"
+  },
+  {
+    "revision": "c7a52cf5c1c4da019ea5",
+    "url": "/myapp/static/js/main.e1e611c2.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
